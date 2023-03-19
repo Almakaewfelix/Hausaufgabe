aa4dcebd9f673b4f5d3e2eccc1f51d3006e5ac39
Almakaewfelix <125497875+Almakaewfelix@users.noreply.github.com>
2023-03-19 20:18:06 +0100

    1

---
+++ Homewor 19.03/src/oneTwo/Task1.java
@@ -0,0 +1,34 @@
+package oneTwo;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String stringStudy = "I study Java Basic!";
+        String stringResultA = stringStudy.contains(Java);
+
+
+        char stringResult = stringStudy.charAt(19);
+
+        System.out.println(stringStudy.length());
+
+        System.out.println(stringResult);
+
+        String stringResult1 = stringStudy.replaceAll(regex; "a", replacement; "o");
+
+        System.out.print(stringResult1);
+
+        String stringResult2 = stringStudy.toUpperCase();
+        System.out.print(stringResult2);
+
+        String stringResult3 = stringStudy.toLowerCase();
+        System.out.print(stringResult3);
+
+        String stringResult4 = stringStudy.substring(9, 12);
+        System.out.print(stringResult4);
+
+
+
+
+
+    }
+}
+
---
+++ Homewor 19.03/src/oneTwo/Task2.java
@@ -0,0 +1,25 @@
+package oneTwo;
+
+public class Task2 {
+    public static void main(String[] args) {
+        byte number0 = 8;
+        int resultInt = 0;
+        Doubl number2 = 2.5;
+        float number3 = 17F;
+
+        resultInt = number0;
+
+        System.out.println(resultInt);
+
+        byte byteVar = 0;
+
+        int intVar = 2000;
+
+        byteVar = (byte)intVar;
+
+        System.out.println(byteVar);
+
+
+
+    }
+}
